931b57380bc132974f5d1e67dcb83ce2103bbf74
klejdihasa <78228827+klejdihasa@users.noreply.github.com>
2024-08-16 00:59:52 +0200

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,6 @@
+const themeToggleButton = document.getElementById('theme-toggle');
+const body = document.body;
+
+themeToggleButton.addEventListener('click', () => {
+    body.classList.toggle('dark-mode');
+});
